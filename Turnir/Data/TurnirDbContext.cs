d9a369cafc61ed8b543b83678cd5faab91ed801b
George <gs.ivanov@abv.bg>
2025-03-13 08:02:35 +0200

    Creating application Turnir

---
+++ Turnir/Data/TurnirDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Turnir.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class TurnirDbContext : IdentityDbContext
+    {
+        public TurnirDbContext(DbContextOptions<TurnirDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Turnir/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Turnir.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
